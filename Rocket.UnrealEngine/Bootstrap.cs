63ff1a565b0407f00ea98282a08785fa9faf2512
Trojaner_ <trojaner25@gmail.com>
2018-04-23 00:30:56 +0300

    Some progress

---
+++ Rocket.UnrealEngine/Bootstrap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rocket.UnrealEngine
+{
+    public class Boostrap
+    {
+        public static void Initialize()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
